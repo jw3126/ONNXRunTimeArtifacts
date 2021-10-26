b6274450d1dceaf1db1705a447c92d66179b88e9
Jan Weidner <jw3126@gmail.com>
2021-10-26 16:00:42 +0200

    works

---
+++ README.md
@@ -0,0 +1,8 @@
+# ONNXRunTimeArtifacts
+
+This repo provides artifacts needed by [ONNXRunTime.jl](https://github.com/jw3126/ONNXRunTime.jl).
+To update the artifacts do:
+
+* Run the script
+* Draft a new release of ONNXRunTimeArtifacts and manually add the binaries from the script
+* Replace ONNXRunTime.jl/Artifacts.toml by the Artifacts.toml produced by the script
---
+++ script.jl
@@ -0,0 +1,70 @@
+using Pkg
+
+version = "1.9.0"
+items = [
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-linux-x64-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
+   ),
+    (
+    artifact_name="onnxruntime_gpu",
+    download_name="onnxruntime-linux-x64-gpu-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
+   ),
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-osx-x64-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "macos"),
+   ),
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-win-x64-$version.zip",
+    platform=Pkg.Artifacts.Platform("x86_64", "windows"),
+   ),
+    (
+    artifact_name="onnxruntime_gpu",
+    download_name="onnxruntime-win-x64-gpu-$version.zip",
+    platform=Pkg.Artifacts.Platform("x86_64", "windows"),
+   ),
+   ]
+
+for item in items
+    filename = item.download_name
+    url = "https://github.com/microsoft/onnxruntime/releases/download/v$version/$filename"
+    if !ispath(filename)
+        @info """Downloading
+        filename = $filename
+        url = $url
+        """
+        download(url, filename)
+    end
+    filestem = splitext(filename)[1]
+    filename_tar = filestem * ".tgz"
+    if !ispath(filename_tar)
+        @info "Converting $filename to tar"
+        if !ispath(filestem)
+            @info "Unzipping $filename"
+            run(`unzip $filename`)
+        end
+        run(`tar czf $filename_tar $filestem`)
+    end
+end
+
+
+using ArtifactUtils
+
+for item in items
+    filestem = splitext(item.download_name)[1]
+    filename_tar = filestem * ".tgz"
+    prefix = "https://github.com/jw3126/ONNXRunTimeArtifacts/releases/download/v0.1.0"
+    artifact_url = "$prefix/$filename_tar"
+    add_artifact!(
+        "Artifacts.toml",
+        item.artifact_name,
+        artifact_url,
+        force=true,
+        platform=item.platform,
+        lazy=true,
+    )
+end
