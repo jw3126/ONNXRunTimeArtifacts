650203f4dea15ea3a0cd6314bd5d8b41dbd529c4
Jan Weidner <jw3126@gmail.com>
2021-10-27 16:36:41 +0200

    tweak script

--- script.jl
+++ script.jl
@@ -1,4 +1,5 @@
 using Pkg
+using ArtifactUtils
 
 version = "1.9.0"
 items = [
@@ -27,49 +28,55 @@ items = [
     download_name="onnxruntime-win-x64-gpu-$version.zip",
     platform=Pkg.Artifacts.Platform("x86_64", "windows"),
    ),
-   ]
+]
 
-for item in items
-    filename = item.download_name
-    url = "https://github.com/microsoft/onnxruntime/releases/download/v$version/$filename"
-    if !ispath(filename)
-        @info """Downloading
-        filename = $filename
-        url = $url
-        """
-        download(url, filename)
-    end
-    filestem = splitext(filename)[1]
-    # Artifacts.jl supports only tar files, so we need to convert zip to tar
-    filename_tar = filestem * ".tgz"
-    if !ispath(filename_tar)
-        @info "Converting $filename to tar"
-        if !ispath(filestem)
-            @info "Unzipping $filename"
-            run(`unzip $filename`)
+function create_tarballs()
+    for item in items
+        @info "Create tarball" artifact_name=item.artifact_name download_name=item.download_name
+        filename = item.download_name
+        url = "https://github.com/microsoft/onnxruntime/releases/download/v$version/$filename"
+        if !ispath(filename)
+            @info """Downloading
+            filename = $filename
+            url = $url
+            """
+            download(url, filename)
         end
-        # On windows dlopen requires the right permissions for the stuff
-        # in the tar file
-        # https://github.com/JuliaLang/julia/issues/38993
-        run(`chmod -R 777 $filestem`)
-        run(`tar czf $filename_tar $filestem`)
+        filestem = splitext(filename)[1]
+        # Artifacts.jl supports only tar files, so we need to convert zip to tar
+        filename_tar = filestem * ".tgz"
+        if !ispath(filename_tar)
+            @info "Converting $filename to tar"
+            if !ispath(filestem)
+                @info "Unzipping $filename"
+                run(`unzip $filename`)
+            end
+            # On windows dlopen requires the right permissions for the stuff
+            # in the tar file
+            # https://github.com/JuliaLang/julia/issues/38993
+            run(`chmod -R 777 $filestem`)
+            run(`tar czf $filename_tar $filestem`)
+        end
     end
 end
 
-
-using ArtifactUtils
-
-for item in items
-    filestem = splitext(item.download_name)[1]
-    filename_tar = filestem * ".tgz"
-    prefix = "https://github.com/jw3126/ONNXRunTimeArtifacts/releases/download/v0.1.1"
-    artifact_url = "$prefix/$filename_tar"
-    add_artifact!(
-        "Artifacts.toml",
-        item.artifact_name,
-        artifact_url,
-        force=true,
-        platform=item.platform,
-        lazy=true,
-    )
+function create_artifact_toml()
+    for item in items
+        @info "adding artifact" artifact_name=item.artifact_name download_name=item.download_name
+        filestem = splitext(item.download_name)[1]
+        filename_tar = filestem * ".tgz"
+        prefix = "https://github.com/jw3126/ONNXRunTimeArtifacts/releases/download/v0.1.0"
+        artifact_url = "$prefix/$filename_tar"
+        add_artifact!(
+            "Artifacts.toml",
+            item.artifact_name,
+            artifact_url,
+            force=true,
+            platform=item.platform,
+            lazy=true,
+        )
+    end
 end
+
+create_tarballs()
+#create_artifact_toml()
