ee7318bd7ac988e390993a203a4f5d12d319278b
Jan Weidner <jw3126@gmail.com>
2022-10-30 22:58:13 +0100

    towards 1.13.1

--- script.jl
+++ script.jl
@@ -1,40 +1,76 @@
 using Pkg
 using ArtifactUtils
 
-version = "1.9.0"
+version = "1.13.1"
 items = [
-    (
-    artifact_name="onnxruntime_cpu",
-    download_name="onnxruntime-linux-x64-$version.tgz",
-    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
-   ),
-    (
-    artifact_name="onnxruntime_gpu",
-    download_name="onnxruntime-linux-x64-gpu-$version.tgz",
-    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
-   ),
-    (
-    artifact_name="onnxruntime_cpu",
-    download_name="onnxruntime-osx-x64-$version.tgz",
-    platform=Pkg.Artifacts.Platform("x86_64", "macos"),
-   ),
     (
     artifact_name="onnxruntime_cpu",
     download_name="onnxruntime-win-x64-$version.zip",
     platform=Pkg.Artifacts.Platform("x86_64", "windows"),
+    version=version,
    ),
     (
     artifact_name="onnxruntime_gpu",
     download_name="onnxruntime-win-x64-gpu-$version.zip",
     platform=Pkg.Artifacts.Platform("x86_64", "windows"),
+    version=version,
+   ),
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-linux-x64-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
+    version=version,
+   ),
+    (
+    artifact_name="onnxruntime_gpu",
+    download_name="onnxruntime-linux-x64-gpu-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "linux"),
+    version=version,
+   ),
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-osx-x86_64-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "macos"),
+    version=version,
+   ),
+    (
+    artifact_name="onnxruntime_cpu",
+    download_name="onnxruntime-osx-universal2-$version.tgz",
+    platform=Pkg.Artifacts.Platform("x86_64", "macos"),
+    version=version,
    ),
 ]
 
+function get_upstream_url(item)
+    "https://github.com/microsoft/onnxruntime/releases/download/v$(item.version)/$(item.download_name)"
+end
+
+function iszip(path)
+    splitext(path)[2] == ".zip"
+end
+
+function get_artifact_url(item)
+    if iszip(item.download_name)
+        get_repacked_url(item)
+    else
+        get_upstream_url(item)
+    end
+end
+
+function get_repacked_url(item)
+    filestem = splitext(item.download_name)[1]
+    filename_tar = filestem * ".tgz"
+    prefix = "https://github.com/jw3126/ONNXRunTimeArtifacts/releases/download/$(item.version)-rc1"
+    "$prefix/$filename_tar"
+end
+
+
 function create_tarballs()
     for item in items
+        iszip(item.download_name) || continue
         @info "Create tarball" artifact_name=item.artifact_name download_name=item.download_name
         filename = item.download_name
-        url = "https://github.com/microsoft/onnxruntime/releases/download/v$version/$filename"
+        url = get_upstream_url(item)
         if !ispath(filename)
             @info """Downloading
             filename = $filename
@@ -75,11 +111,8 @@ end
 
 function create_artifact_toml()
     for item in items
-        @info "adding artifact" artifact_name=item.artifact_name download_name=item.download_name
-        filestem = splitext(item.download_name)[1]
-        filename_tar = filestem * ".tgz"
-        prefix = "https://github.com/jw3126/ONNXRunTimeArtifacts/releases/download/v1.9.0-rc4"
-        artifact_url = "$prefix/$filename_tar"
+        artifact_url = get_artifact_url(item)
+        @info "adding artifact" artifact_name=item.artifact_name download_name=item.download_name artifact_url=artifact_url
         add_artifact!(
             "Artifacts.toml",
             item.artifact_name,
@@ -91,5 +124,5 @@ function create_artifact_toml()
     end
 end
 
-#create_tarballs()
+# create_tarballs()
 create_artifact_toml(); cp("Artifacts.toml", joinpath(homedir(), ".julia", "dev", "ONNXRunTime", "Artifacts.toml"), force=true)
